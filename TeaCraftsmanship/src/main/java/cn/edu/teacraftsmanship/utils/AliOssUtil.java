ced434ab45a238a8fc3702acdc6e43c4235e9687
huang <h18395303098@163.com>
2025-08-10 14:53:25 +0800

    再次提交

---
+++ TeaCraftsmanship/src/main/java/cn/edu/teacraftsmanship/utils/AliOssUtil.java
@@ -0,0 +1,47 @@
+package cn.edu.teacraftsmanship.utils;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+
+import java.io.InputStream;
+
+public class AliOssUtil {
+    // 阿里云OSS的配置信息，包括Endpoint、AccessKeyId、AccessKeySecret等
+    private static final String ENDPOINT = "";
+    private static final String ACCESS_KEY_ID = "";
+    private static final String ACCESS_KEY_SECRET = "";
+    private static final String BUCKET_NAME = "";
+
+    //上传文件,返回文件的公网访问地址
+    public static String uploadFile(String objectName, InputStream inputStream){
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(ENDPOINT,ACCESS_KEY_ID,ACCESS_KEY_SECRET);
+        //公文访问地址
+        String url = "";
+        try {
+            // 创建存储空间。
+            ossClient.createBucket(BUCKET_NAME);
+            ossClient.putObject(BUCKET_NAME, objectName, inputStream);
+            url = "https://"+BUCKET_NAME+"."+ENDPOINT.substring(ENDPOINT.lastIndexOf("/")+1)+"/"+objectName;
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+        return url;
+    }
+}
